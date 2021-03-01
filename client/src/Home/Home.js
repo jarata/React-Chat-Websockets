5f15c2adf4f3dea11725c5870b3eb35d9fd5098b
Justin A <jarata@gmail.com>
2021-03-01 12:15:40 -0800

    Room Component started building room component to direct to chat rooms

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
+    // Room State, default is none
+    const [roomName, setRoomName] = useState("");
+    // Room Change handler, sets room name to input when submitted
+    const handleRoomChanges = (event) => {
+        setRoomName(event.target.value)
+    }
+    // Home Component Render, input field for room name and join link for updated roomName
     return (
-        <div>
-            Home
+        <div className="homeContainer">
+            <input
+                className="homeContainerInput"
+                type="text"
+                value={roomName}
+                placeholder="Room ID"
+                onChange={handleRoomChanges}
+            />
+            <Link to={`/${roomName}`} className="homeContainerLink">
+                Join Room
+            </Link>
         </div>
     )
 }
