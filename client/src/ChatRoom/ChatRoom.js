9142f3cabc6a708b2b66fb0fa58a48f849e939a2
Justin A <jarata@gmail.com>
2021-03-01 13:10:10 -0800

    Chat Room Component added message submit handler and added reference to useChat hook that will handle messages from server

--- client/src/ChatRoom/ChatRoom.js
+++ client/src/ChatRoom/ChatRoom.js
@@ -7,12 +7,21 @@ const ChatRoom = (props) => {
     console.log("Room ID:", roomId);
     // State to update new message from input
     const [ newMessage, setNewMessage ] = useState("");
+    // State that connects to server and handles messages
+    const { messages, submitMessage } = useChat(roomId);
     // Handle message input and setState
     const handleNewMessages = (event) => {
         console.log("New Message Handler");
         setNewMessage(event.target.value);
     }
+    // Handles submitted message and resets message input
+    const handleSubmitMessage = () => {
+        console.log("Message Submit");
+        submitMessage(newMessage);
+        setNewMessage("");
+    }
     console.log("Handle New Message", newMessage);
+    console.log("Messages:", messages);
     // Chat Room Render, displays chat from room Id as well as inputs for sending messages to room
     return (
         <div className="chatRoomContainer">
@@ -28,7 +37,7 @@ const ChatRoom = (props) => {
                 value={newMessage}
                 placeholder="New Message..."
             />
-            <button className="chatRoomMessageSubmit">
+            <button className="chatRoomMessageSubmit" onClick={handleSubmitMessage}>
                 Send
             </button>
         </div>
