3ae993066273a80c1ab4ff543c7e4e81db26119d
Justin A <jarata@gmail.com>
2021-03-01 13:01:32 -0800

    Chat Room Component added message handler and useState for new messages

--- client/src/ChatRoom/ChatRoom.js
+++ client/src/ChatRoom/ChatRoom.js
@@ -2,8 +2,18 @@ import { useState } from "react";
 
 const ChatRoom = (props) => {
     console.log("Chat Room Props:", props.match.params);
+    // Room Id from url
     const { roomId } = props.match.params;
     console.log("Room ID:", roomId);
+    // State to update new message from input
+    const [ newMessage, setNewMessage ] = useState("");
+    // Handle message input and setState
+    const handleNewMessages = (event) => {
+        console.log("New Message Handler");
+        setNewMessage(event.target.value);
+    }
+    console.log("Handle New Message", newMessage);
+    // Chat Room Render, displays chat from room Id as well as inputs for sending messages to room
     return (
         <div className="chatRoomContainer">
             <h1>Room: {roomId}</h1>
@@ -14,6 +24,9 @@ const ChatRoom = (props) => {
             </div>
             <textarea
                 className="chatRoomNewMessage"
+                onChange={handleNewMessages}
+                value={newMessage}
+                placeholder="New Message..."
             />
             <button className="chatRoomMessageSubmit">
                 Send
