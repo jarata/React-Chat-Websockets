97a8fb03f8e909e8f8d5aae2136abb1d1a711ded
Justin A <jarata@gmail.com>
2021-03-01 12:50:04 -0800

    Chat Room Component started structure for chat room component with props

--- client/src/ChatRoom/ChatRoom.js
+++ client/src/ChatRoom/ChatRoom.js
@@ -1,7 +1,23 @@
-const ChatRoom = () => {
+import { useState } from "react";
+
+const ChatRoom = (props) => {
+    console.log("Chat Room Props:", props.match.params);
+    const { roomId } = props.match.params;
+    console.log("Room ID:", roomId);
     return (
-        <div>
-            ChatRoom
+        <div className="chatRoomContainer">
+            <h1>Room: {roomId}</h1>
+            <div className="chatRoomMessagesContainer">
+                <ol className="chatRoomMessages">
+                    messages
+                </ol>
+            </div>
+            <textarea
+                className="chatRoomNewMessage"
+            />
+            <button className="chatRoomMessageSubmit">
+                Send
+            </button>
         </div>
     )
 }
