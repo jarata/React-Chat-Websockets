339ca9e52f8b4a78488e564870f13d6e33a7fff8
Justin A <jarata@gmail.com>
2021-03-01 19:55:13 -0800

    Chat Room Component

--- client/src/ChatRoom/ChatRoom.js
+++ client/src/ChatRoom/ChatRoom.js
@@ -1,34 +1,37 @@
-import { useState } from "react";
+import { useState } from 'react';
+import useChat from '../lib/useChat';
 
 const ChatRoom = (props) => {
     console.log("Chat Room Props:", props.match.params);
     // Room Id from url
     const { roomId } = props.match.params;
-    console.log("Room ID:", roomId);
     // State to update new message from input
     const [ newMessage, setNewMessage ] = useState("");
     // State that connects to server and handles messages
     const { messages, submitMessage } = useChat(roomId);
     // Handle message input and setState
     const handleNewMessages = (event) => {
-        console.log("New Message Handler");
         setNewMessage(event.target.value);
     }
     // Handles submitted message and resets message input
     const handleSubmitMessage = () => {
-        console.log("Message Submit");
         submitMessage(newMessage);
         setNewMessage("");
     }
-    console.log("Handle New Message", newMessage);
-    console.log("Messages:", messages);
     // Chat Room Render, displays chat from room Id as well as inputs for sending messages to room
     return (
         <div className="chatRoomContainer">
             <h1>Room: {roomId}</h1>
             <div className="chatRoomMessagesContainer">
                 <ol className="chatRoomMessages">
-                    messages
+                    {messages.map((message, index) => (
+                        <li
+                            key={index}
+                            className={`chatRoomMessage ${message.ownedByCurrentUser ? "myMessage" : "receivedMessage"}`}
+                        >
+                            {message.body}
+                        </li>
+                    ))}
                 </ol>
             </div>
             <textarea
