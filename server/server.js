e8b13516696d15fe6e8ac33d285fa5cee1d527e4
Justin A <jarata@gmail.com>
2021-03-05 11:53:20 -0800

    Server added functionality for join, listen, and leaving a server

--- server/server.js
+++ server/server.js
@@ -9,9 +9,19 @@ const PORT = 4000;
 const NEW_CHAT_EVENT = "newChatEvent";
 
 io.on("connection", (socket) => {
-
-})
+    // Join
+    const { roomId } = socket.handshake.query;
+    socket.join(roomId);
+    // Listen
+    socket.on(NEW_CHAT_EVENT, (data) => {
+        io.on(roomId).emit(NEW_CHAT_EVENT, data);
+    });
+    // Leave
+    socket.on("Disconnect", () => {
+        socket.leave(roomId);
+    })
+});
 
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
+    console.log(`Server running on ${PORT}`);
+});
